fc2ca9b8809f7d31cbc1e70b71c04caa5c5c3586
gennadiymoroz <g.moroz@adtelligent.com>
2025-10-21 22:36:00 +0200

    onboarding pt 2

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -108,7 +108,7 @@ export default {
         })
         .catch(err => {
           console.error(err)
-          this.error = 'Invalid credentials. Please check your email and password.'
+          this.error = err.response?.data?.error?.message
         })
         .finally(() => {
           this.loading = false
